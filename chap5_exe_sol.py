8750e7064e325adf04fec973514b85ff33a3d51e
Sanglae Kim <nova0302@hotmail.com>
2019-06-04 16:59:51 +0900

    errortar fixed

--- chap5_exe_sol.py
+++ chap5_exe_sol.py
@@ -3,23 +3,26 @@ class Calculator:
     def __init__(self, lst):
         self.lst = lst
 
-    def getTotal(self):
-        return sum(self.lst)
+    def sum(self):
+        total = 0;
+        for n in lst:
+            total += n
+        return total
 
     def avg(self):
-        return self.getTotal()/len(self.lst)
+        return self.sum()/len(self.lst)
 
 if __name__ == "__main__":
     print('Hello World')
     cal1 = Calculator([1,2,3,4,5])
-    print(cal1.getTotal())
+    print(cal1.sum())
     print(cal1.avg())
 
     cal1 = Calculator([6,7,8,9,10])
-    print(cal1.getTotal())
+    print(cal1.sum())
     print(cal1.avg())
 
 # main.py
 from calculator import Calculator
 cal1 = Calculator([1,2,3,4,5])
-print(cal1.getTotal())
+print(cal1.sum())
